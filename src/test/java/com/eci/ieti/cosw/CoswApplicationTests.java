6ade6c091d87b61030ba6bd886d68c44068f67b8
Ricar8o <andres.martinez-d@mail.escuelaing.edu.co>
2021-03-22 18:14:54 -0500

    Repository copied and updated to JAVA 11

---
+++ src/test/java/com/eci/ieti/cosw/CoswApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eci.ieti.cosw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoswApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
