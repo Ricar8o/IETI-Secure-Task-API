ece184d6d802df9d6688034472dc9b704490f46f
Ricar8o <andres.martinez-d@mail.escuelaing.edu.co>
2021-03-25 13:19:19 -0500

    User FrontEnd Secure

--- frontend-consumption/src/components/UserApp.js
+++ frontend-consumption/src/components/UserApp.js
@@ -9,33 +9,44 @@ export class UserApp extends React.Component{
         super(props);
         this.state = {
             userList: [],
+            nextId: 0
         };
     }
 
     componentDidMount() {
+
+        this.axios = axios.create({
+            baseURL: 'http://localhost:8080/api/',
+            timeout: 1000,
+            headers: {'Authorization': 'Bearer ' + localStorage.getItem("token")}
+        });
     
-        axios.get('http://localhost:8080/api/user',{ headers: {'Authorization': 'Bearer ' + localStorage.getItem("token")}})
+        this.axios.get('http://localhost:8080/api/user')
         .then(response => response.data)
         .then(data => {
             let userList = [];
+            let actualId = 0;
             data.forEach(function (user) {
                 userList.push(
                     user
                 )
+                if(user.id>actualId){
+                    actualId = user.id;
+                }
     
             });
-            this.setState({userList: userList});
+            this.setState({userList: userList, nextId:actualId+1});
         })
         .catch(function (error) {
             console.log(error);
         });
-      }
+    }
 
     render(){
         return(
             <div>
-                <UserList userList={this.state.userList}/>
-                <UserForm/>
+                <UserList userList={this.state.userList} />
+                <UserForm actualId={this.state.nextId} axios={this.axios}/>
             </div>
         );
     }
--- frontend-consumption/src/components/UserForm.js
+++ frontend-consumption/src/components/UserForm.js
@@ -1,11 +1,84 @@
 import React from 'react';
+import swal from 'sweetalert';
+import './Style.css';
 
 export class UserForm extends React.Component{
 
+    constructor(props) {
+        super(props);
+        this.state = {firstname: '', lastname: '', username:'', email:'', password:''};
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(event) {
+        // event.preventDefault();
+
+        
+        if (!this.state.firstname.length || !this.state.lastname.length || !this.state.username.length || !this.state.email.length || !this.state.password.length){
+            swal({
+                title: "Wrong Data",
+                icon: "error"
+            })
+        }else{
+            this.props.axios.post('http://localhost:8080/api/user', {
+                id: this.props.actualId,
+                firstname: this.state.firstname,
+                lastname: this.state.lastname,
+                username: this.state.username,
+                email: this.state.email,
+                password: this.state.password
+            })
+            .then(function (response) {
+                console.log(response.data);
+                swal({
+                    title: "Post Request Done :)",
+                    icon: "success"
+                })
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+            
+        }
+        
+    }
+
+    handleChange(event){
+        this.setState({ [event.target.name] : event.target.value});    
+    }
+
     render(){
         return(
-            <div>
-                Hola
+            <div className="userForm">
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Firstname:
+                        <input name="firstname" type="text" value={this.state.firstname} onChange={this.handleChange} />         
+                    </label>
+
+                    <label>
+                        Lastname:
+                        <input name="lastname" type="text" value={this.state.lastname} onChange={this.handleChange} />         
+                    </label>
+
+                    <label>
+                        Username:
+                        <input name="username" type="text" value={this.state.username} onChange={this.handleChange} />         
+                    </label>
+
+                    <label>
+                        Email:
+                        <input name="email" type="text" value={this.state.email} onChange={this.handleChange} />         
+                    </label>
+
+                    <label>
+                        Password:
+                        <input name="password" type="password" value={this.state.password} onChange={this.handleChange} />         
+                    </label>
+
+                    <input type="submit" value="Add" />
+                </form>
             </div>
         );
     }
